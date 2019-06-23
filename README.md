7ef54670079b20b709ce05b1e1fda9042c48fbc5
tomic <violeticus@gmail.com>
2019-06-23 15:48:04 +0200

    Init repository Flyway added

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# HerokuDemo
+# Spring Boot Heroku Flyway demo
+
+Example showing how to conect Spring Boot App to Heroku database with Flyway library used as database versioning.
---
+++ src/main/java/com/heroku/demo/RecordRepository.java
@@ -0,0 +1,8 @@
+package com.heroku.demo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RecordRepository extends JpaRepository<Record, Long> {
+}
